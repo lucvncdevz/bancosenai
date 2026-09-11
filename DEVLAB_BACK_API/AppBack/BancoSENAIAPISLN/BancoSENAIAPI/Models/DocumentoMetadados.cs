b1031f55f150cb7f8aeefbf24d5e1af9ec7f655a
lucasvnunes-lgtm <lucas.v.nunes@aluno.senai.br>
2026-09-11 08:26:35 -0300

    feature(documentos): criação de controller e model de documentos

---
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/DocumentoMetadados.cs
@@ -0,0 +1,11 @@
+﻿namespace BancoSENAIAPI.Models
+{
+    public class DocumentoMetadados
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Extensao { get; set;}
+        public string Caminho { get; set; }
+        public string CodigoClient { get; set;}
+    }
+}
