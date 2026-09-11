b07c96064dd42966788ffd866feb501d14f658c4
lucasvnunes-lgtm <lucas.v.nunes@aluno.senai.br>
2026-09-11 08:53:52 -0300

    feature(Documentos: validação arquivo)

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
@@ -15,7 +15,10 @@ namespace BancoSENAIAPI.Controllers
         [HttpPost("upload/{codigoClient}")]
         public async Task<IActionResult> AnexarArquivo(int codigoClient, IFormFile arquivo) 
         {
-               
+            if(arquivo == null || arquivo.Length == 0)
+            {
+                return BadRequest("Nenhum arquivo foi enviado.");
+            }   
         }
     }
 }
