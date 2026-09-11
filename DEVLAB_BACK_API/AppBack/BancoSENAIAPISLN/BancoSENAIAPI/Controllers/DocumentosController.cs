0647d98f04c39f318af045e5e4a692c919468ddd
lucasvnunes-lgtm <lucas.v.nunes@aluno.senai.br>
2026-09-11 08:37:24 -0300

    feature(documetos: simulação banco de dados)

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
@@ -6,6 +6,10 @@ namespace BancoSENAIAPI.Controllers
     [Route("api/v1/[controller]")]
     public class DocumentosController : Controller
     {
-        
+        private readonly string _caminhoRaiz = Path.Combine(Directory.GetCurrentDirectory(), "clienteArquivos");
+
+        private static List<Models.DocumentoMetadados> _documentosMetadados = new List<Models.DocumentoMetadados>();
+
+        private static int _nextId = 1;
     }
 }
