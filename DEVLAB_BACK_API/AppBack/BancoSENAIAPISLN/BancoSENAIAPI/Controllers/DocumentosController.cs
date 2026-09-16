27ffb576e707718a34c16afd71c477aea5e7a609
democritomenezes-ship-it <democrito.menezes@aluno.senai.br>
2026-09-16 08:42:34 -0300

    Prática 12 - Banco Senai 02: Fase 02/documents CRUD

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
@@ -1,74 +1,85 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BancoSENAIAPI.DB;
+using BancoSENAIAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BancoSENAIAPI.Controllers
 {
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class DocumentosController : Controller
+    public class DocumentController : ControllerBase
     {
-        private readonly string _caminhoRaiz = Path.Combine(Directory.GetCurrentDirectory(), "clienteArquivos");
-
-        private static List<Models.DocumentoMetadados> _documentosMetadados = new List<Models.DocumentoMetadados>();
+        private readonly string _caminhoRaiz =
+            Path.Combine(Directory.GetCurrentDirectory(), "ClienteArquivo");
 
         private static int _nextId = 1;
-        [HttpPost("upload/{codigoClient}")]
-        public async Task<IActionResult> AnexarArquivo(int codigoClient, IFormFile arquivo)
+
+        [HttpPost("upload/{codigoCliente}")]
+        public async Task<IActionResult> PostArquivo(
+            int codigoCliente,
+            IFormFile arquivo)
         {
+            double tamanhoMax = 2 * 1024 * 1024;
+
             if (arquivo == null || arquivo.Length == 0)
             {
-                return BadRequest("Nenhum arquivo foi enviado.");
+                return BadRequest("Nenhum arquivo encontrado");
             }
 
-            // R06F - Limite máximo de 2 MB
-            const long limiteMaximo = 2 * 1024 * 1024;
-
-            if (arquivo.Length > limiteMaximo)
+           /* if (arquivo.Length > tamanhoMax)
             {
-                return BadRequest("O arquivo excede o limite máximo permitido de 2 MB.");
+                return BadRequest("Tamanho do arquivo passou de 2MB, então não pode ser enviado");
+            }*/
+
+            if (!Banco._cliente.Any(e => e.CodigoClient == codigoCliente))
+            {
+                return NotFound("Nenhum cliente encontrado");
             }
 
-            // R06G - Extensões permitidas
-            string extensao = Path.GetExtension(arquivo.FileName).ToLowerInvariant();
+           string extensao = Path.GetExtension(arquivo.FileName).ToLower();
 
-            string[] extensoesPermitidas = { ".pdf", ".jpg", ".png" };
-
-            if (!extensoesPermitidas.Contains(extensao))
+           /* if (extensao != ".png" &&
+                extensao != ".jpg" &&
+                extensao != ".pdf")
             {
                 return BadRequest(
-                    "Formato de arquivo não permitido. " +
-                    "Apenas arquivos .pdf, .jpg e .png são aceitos."
-                );
-            }
+                    "Somente podem ser enviados arquivos do tipo png, jpg ou pdf");
+            }*/
 
-            string pastaClient = Path.Combine(
+            string pastaCliente = Path.Combine(
                 _caminhoRaiz,
-                codigoClient.ToString()
-            );
+                codigoCliente.ToString());
 
-            if (!Directory.Exists(pastaClient))
+            if (!Directory.Exists(pastaCliente))
             {
-                Directory.CreateDirectory(pastaClient);
+                Directory.CreateDirectory(pastaCliente);
             }
 
-            string nomeOriginal = Path.GetFileNameWithoutExtension(arquivo.FileName);
-            string novoNome = $"{codigoClient}.{nomeOriginal}_{Guid.NewGuid()}{extensao}";
-            string caminhoFinal = Path.Combine(pastaClient, novoNome);
+            string nomeOriginal =
+                Path.GetFileNameWithoutExtension(arquivo.FileName);
 
-            using (var stream = new FileStream(caminhoFinal, FileMode.Create))
+           string novoNome =
+                $"{codigoCliente}_{nomeOriginal}_{Guid.NewGuid()}{extensao}";
+
+            string caminhoFinal =
+                Path.Combine(pastaCliente, novoNome);
+
+            using (var stream = new FileStream(
+                caminhoFinal,
+                FileMode.Create))
             {
                 await arquivo.CopyToAsync(stream);
             }
 
-            var documentoMetadados = new Models.DocumentoMetadados()
+            var documentoNovo = new DocumentoMetadados
             {
                 Id = _nextId++,
                 Name = nomeOriginal,
                 Extensao = extensao,
-                Caminho = nomeOriginal,
-                CodigoClient = codigoClient
+                Caminho = caminhoFinal,
+                CodigoClient = codigoCliente
             };
 
-            _documentosMetadados.Add(documentoMetadados);
+            Banco._document.Add(documentoNovo);
 
             return Ok(new
             {
@@ -76,6 +87,87 @@ namespace BancoSENAIAPI.Controllers
             });
         }
 
+        [HttpGet("listagem/{codigoCliente}")]
+        public IActionResult ListarDocumentos(int codigoCliente)
+        {
+            if (!Banco._cliente.Any(e => e.CodigoClient == codigoCliente))
+            {
+                return NotFound("Nenhum cliente encontrado");
+            }
 
+            var documentos = Banco._document
+                .Where(d => d.CodigoClient == codigoCliente)
+                .ToList();
+
+            return Ok(documentos);
+        }
+
+        [HttpGet("cliente/{codigoCliente}/download/{id}")]
+        public async Task<IActionResult> DownloadArquivo(
+            int id,
+            int codigoCliente)
+        {
+            var documento = Banco._document
+                .FirstOrDefault(e => e.Id == id);
+
+            if (documento == null)
+            {
+                return NotFound("Nenhum arquivo encontrado");
+            }
+
+            if (documento.CodigoClient != codigoCliente)
+            {
+                return BadRequest("Você não pode ver esse arquivo");
+            }
+
+            if (!System.IO.File.Exists(documento.Caminho))
+            {
+                return NotFound("Arquivo não encontrado no servidor");
+            }
+
+            var fileBytes =
+                await System.IO.File.ReadAllBytesAsync(documento.Caminho);
+
+            string contentType = documento.Extensao.ToLower() switch
+            {
+                ".png" => "image/png",
+                ".jpg" => "image/jpeg",
+                ".pdf" => "application/pdf",
+                _ => "application/octet-stream"
+            };
+
+            return File(
+                fileBytes,
+                contentType,
+                documento.Name + documento.Extensao);
+        }
+
+        [HttpDelete("cliente/{codigoCliente}/excluir/{id}")]
+        public IActionResult DeleteDocument(
+            int id,
+            int codigoCliente)
+        {
+            var documento = Banco._document
+                .FirstOrDefault(e => e.Id == id);
+
+            if (documento == null)
+            {
+                return NotFound("Nenhum arquivo encontrado");
+            }
+
+            if (documento.CodigoClient != codigoCliente)
+            {
+                return BadRequest("Você não pode excluir esse arquivo");
+            }
+
+            Banco._document.Remove(documento);
+
+            if (System.IO.File.Exists(documento.Caminho))
+            {
+                System.IO.File.Delete(documento.Caminho);
+            }
+
+            return NoContent();
+        }
     }
 }
---
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/DB/Banco.cs
@@ -0,0 +1,102 @@
+﻿using BancoSENAIAPI.Models;
+
+namespace BancoSENAIAPI.DB
+{
+    public class Banco
+    {
+        public static List<Carteira> _carteira = new List<Carteira>
+        {
+            new Carteira
+            {
+                NumeroCarteira = 101,
+                NomeCarteira = "Agro",
+                ApetiteCarteira = 10000
+            },
+
+            new Carteira
+            {
+                NumeroCarteira = 102,
+                NomeCarteira = "Tech",
+                ApetiteCarteira = 10000
+            },
+
+            new Carteira
+            {
+                NumeroCarteira = 103,
+                NomeCarteira = "Pop",
+                ApetiteCarteira = 10000
+            }
+        };
+
+
+        public static List<Agencia> _agencias = new List<Agencia>
+        {
+            new Agencia
+            {
+                NumeroAgencia = 1001,
+                Cidade = "Aracaju",
+                SiglaEstado = "SE"
+            },
+
+            new Agencia
+            {
+                NumeroAgencia = 2002,
+                Cidade = "São Paulo",
+                SiglaEstado = "SP"
+            },
+
+            new Agencia
+            {
+                NumeroAgencia = 3003,
+                Cidade = "Salvador",
+                SiglaEstado = "BA"
+            }
+        };
+
+        public static List<Client> _cliente = new List<Client>
+        {
+            new Client
+            {
+                CodigoClient = 1,
+                NomeClient = "Felipe",
+                CPF = "00900900900",
+                Sexo = "M",
+                Enderco = "Rua A",
+                Cidade = "Aracaju",
+                Estado = "SE",
+                SaldoTotal = 1000,
+                NumeroAgencia = 1001
+            },
+
+            new Client
+            {
+                CodigoClient = 2,
+                NomeClient = "Caio",
+                CPF = "00800800800",
+                Sexo = "M",
+                Enderco = "Rua B",
+                Cidade = "Aracaju",
+                Estado = "SE",
+                SaldoTotal = 1000,
+                NumeroAgencia = 2002
+            },
+
+            new Client
+            {
+                CodigoClient = 3,
+                NomeClient = "Paulo",
+                CPF = "00700700700",
+                Sexo = "M",
+                Enderco = "Rua C",
+                Cidade = "Aracaju",
+                Estado = "SE",
+                SaldoTotal = 1000,
+                NumeroAgencia = 3003
+            }
+        };
+
+        public static List<DocumentoMetadados> _document =
+            new List<DocumentoMetadados>();
+
+    }
+}
