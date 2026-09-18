f58bf8d87db60731f8cd2bc32018bbe2de1fbff5
democritomenezes-ship-it <democrito.menezes@aluno.senai.br>
2026-09-18 08:16:48 -0300

    feature(documents): teste funcionando

--- DEVLAB_BACK_API/AppFront/Documento/documento.js
+++ DEVLAB_BACK_API/AppFront/Documento/documento.js
@@ -1,4 +1,4 @@
-const URL_API = 'http://localhost:7081/api/v1/Documento';
+const URL_API = 'https://localhost:7081/api/v1/Document';
 
 async function enviarDocumento() {
     const codigoCliente = document.getElementById("codigoCliente").value;
@@ -23,5 +23,7 @@ async function enviarDocumento() {
         alert("Documento enviado com sucesso !");
         document.getElementById("codigoCliente").value = "";
         document.getElementById("arquivo").value = "";
+    } else{
+        alert("Deu erro")
     }
 }
